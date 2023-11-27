de2dbdf4ffefa2320974fcd726a726a71e6b02dc
lamphandsome <lamphamxxx04@gmail.com>
2023-11-27 08:29:43 +0700

    .

--- src/App.js
+++ src/App.js
@@ -1,20 +1,9 @@
 import React from "react";
-// import "./index.css";
 
-import Navbar from "./Navbar";
-import PricingHeader from "./PricingHeader.js";
-import Plans from "./Plans";
-import Footer from "./Footer";
-import PlanCard from "./PlanCard.js";
-import ItemList from "./itemlist.js"
-import Myapp from "./ProductList.js";
-import ProductList from "./ProductList.js";
-import "./App.css";
-import ListSanPham from "./Web3.js";
-import WebHeader1 from "./WebHeader.js";
-import "./web.css"
-import Middle from "./WebMiddle.js";
-import groupMembers from "./WebFooter.js";
+import ListSanPham from "./webnhomlam/Web3.js";
+import WebHeader1 from "./webnhomlam/WebHeader.js";
+import "./webnhomlam/web.css"
+import Middle from "./webnhomlam/WebMiddle.js";
 export default function App() {
   return (
     <div className="App" id="top">
@@ -28,7 +17,6 @@ export default function App() {
       <WebHeader1 />
       <Middle />
       <ListSanPham />
-      <groupMembers />
     </div>
   );
 }
